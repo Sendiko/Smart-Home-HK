d7729847d7a46567043bb22a5818b4f38919a4cd
Sendiko <sarangtawon897@gmail.com>
2025-11-05 14:14:03 +0700

    chore: removed platform-specific code and refactored `App` structure for cleaner architecture.

--- composeApp/src/iosMain/kotlin/id/co/hasilkarya/smarthome/smarthomehasilkarya/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/id/co/hasilkarya/smarthome/smarthomehasilkarya/MainViewController.kt
@@ -1,5 +1,6 @@
 package id.co.hasilkarya.smarthome.smarthomehasilkarya
 
 import androidx.compose.ui.window.ComposeUIViewController
+import id.co.hasilkarya.smarthome.smarthomehasilkarya.core.App
 
 fun MainViewController() = ComposeUIViewController { App() }
