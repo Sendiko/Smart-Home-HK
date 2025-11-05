e990ea14428d14007f5c5e2374a50ab5a1f07cc7
Sendiko <sarangtawon897@gmail.com>
2025-11-05 19:25:10 +0700

    chore: added reusable composable components for `PrimaryButton`, `CustomTextField`, and `SecureTextField` in login presentation.

---
+++ composeApp/src/commonMain/kotlin/id/co/hasilkarya/smarthome/login/presentation/components/CustomTextField.kt
@@ -0,0 +1,93 @@
+package id.co.hasilkarya.smarthome.login.presentation.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import id.co.hasilkarya.smarthome.core.theme.BrokenWhite
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import smarthomehasilkarya.composeapp.generated.resources.Res
+import smarthomehasilkarya.composeapp.generated.resources.clear_field
+import smarthomehasilkarya.composeapp.generated.resources.email
+import smarthomehasilkarya.composeapp.generated.resources.email_icon
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    hint: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        shape = RoundedCornerShape(8.dp),
+        leadingIcon = leadingIcon,
+        textStyle = TextStyle(
+            color = BrokenWhite
+        ),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = BrokenWhite,
+            unfocusedBorderColor = BrokenWhite.copy(0.11f),
+            focusedLeadingIconColor = BrokenWhite,
+            unfocusedLeadingIconColor = BrokenWhite.copy(0.85f),
+            focusedTextColor = BrokenWhite,
+            focusedTrailingIconColor = BrokenWhite,
+            unfocusedTrailingIconColor = BrokenWhite.copy(0.85f),
+        ),
+        placeholder = {
+            Text(hint, color = BrokenWhite.copy(0.85f))
+        },
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = value.isNotEmpty(),
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                IconButton(
+                    onClick = { onValueChange("") }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = stringResource(Res.string.clear_field),
+                        tint = BrokenWhite
+                    )
+                }
+            }
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomTextFieldPreview() {
+    CustomTextField(
+        modifier = Modifier.padding(16.dp),
+        value = "",
+        onValueChange = {  },
+        hint = "Email",
+        leadingIcon = {
+            Icon(
+                painter = painterResource(Res.drawable.email),
+                contentDescription = stringResource(Res.string.email_icon),
+                tint = BrokenWhite
+            )
+        }
+    )
+}
---
+++ composeApp/src/commonMain/kotlin/id/co/hasilkarya/smarthome/login/presentation/components/SecureTextField.kt
@@ -0,0 +1,100 @@
+package id.co.hasilkarya.smarthome.login.presentation.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import id.co.hasilkarya.smarthome.core.theme.BrokenWhite
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import smarthomehasilkarya.composeapp.generated.resources.Res
+import smarthomehasilkarya.composeapp.generated.resources.lock
+import smarthomehasilkarya.composeapp.generated.resources.lock_icon
+import smarthomehasilkarya.composeapp.generated.resources.password_not_visible
+import smarthomehasilkarya.composeapp.generated.resources.password_visible
+
+@Composable
+fun SecureTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    onVisibilityChange: (Boolean) -> Unit,
+    hint: String,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    isVisible: Boolean,
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        leadingIcon = leadingIcon,
+        shape = RoundedCornerShape(8.dp),
+        placeholder = {
+            Text(hint, color = BrokenWhite.copy(0.85f))
+        },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = BrokenWhite,
+            unfocusedBorderColor = BrokenWhite.copy(0.11f),
+            focusedLeadingIconColor = BrokenWhite,
+            unfocusedLeadingIconColor = BrokenWhite.copy(0.85f),
+            focusedTextColor = BrokenWhite,
+            focusedTrailingIconColor = BrokenWhite,
+            unfocusedTrailingIconColor = BrokenWhite.copy(0.85f),
+        ),
+        textStyle = TextStyle(
+            color = BrokenWhite
+        ),
+        visualTransformation = if (isVisible)
+            VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            if (isVisible) {
+                IconButton(
+                    onClick = { onVisibilityChange(!isVisible) }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Visibility,
+                        contentDescription = stringResource(Res.string.password_visible),
+                    )
+                }
+            } else {
+                IconButton(
+                    onClick = { onVisibilityChange(!isVisible) }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Visibility,
+                        contentDescription = stringResource(Res.string.password_not_visible),
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SecureTextFieldPreview() {
+    SecureTextField(
+        value = "",
+        onValueChange = {  },
+        onVisibilityChange = {  },
+        hint = "Password",
+        isVisible = true,
+        leadingIcon = {
+            Icon(
+                painter = painterResource(Res.drawable.lock),
+                contentDescription = stringResource(Res.string.lock_icon),
+            )
+        }
+    )
+}
