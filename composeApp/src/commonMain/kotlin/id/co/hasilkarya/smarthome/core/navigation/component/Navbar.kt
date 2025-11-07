cd9acbad9e7a758a851427f3288c12813a877d61
Sendiko <sarangtawon897@gmail.com>
2025-11-07 11:21:21 +0700

    feat(navigation): added `Navbar` and `MainContainer` components
    Implemented `Navbar` for bottom navigation and integrated it into the `MainContainer` scaffold. Added support for navigation with `NavHost` and composable destinations for Home, History, Automation, and Profile.

---
+++ composeApp/src/commonMain/kotlin/id/co/hasilkarya/smarthome/core/navigation/component/Navbar.kt
@@ -0,0 +1,57 @@
+package id.co.hasilkarya.smarthome.core.navigation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import id.co.hasilkarya.smarthome.core.navigation.data.mainNavigations
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Navbar(
+    navController: NavController,
+) {
+    val selectedIndex = rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Surface(
+        modifier = Modifier.padding(16.dp),
+        color = Color.Black.copy(0.5f),
+        shape = CircleShape
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            mainNavigations.forEachIndexed { index, data ->
+                NavbarItem(
+                    data = data,
+                    onNavigate = {
+                        selectedIndex.value = index
+                        navController.navigate(data.route)
+                    },
+                    isSelected = selectedIndex.value == index
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NavbarPreview() {
+    Navbar(rememberNavController())
+}
