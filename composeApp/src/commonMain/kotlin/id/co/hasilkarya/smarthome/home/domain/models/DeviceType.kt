28211045891355b4394bb638d72bd7e7c732e2ed
Sendiko <sarangtawon897@gmail.com>
2025-11-07 08:14:17 +0700

    feat(domain): added domain models for `Device`, `DeviceType`, `Room`, and `Home`
    Added foundational domain models `Device`, `DeviceType`, `Room`, and `Home` to represent key entities for smart home management.

---
+++ composeApp/src/commonMain/kotlin/id/co/hasilkarya/smarthome/home/domain/models/DeviceType.kt
@@ -0,0 +1,6 @@
+package id.co.hasilkarya.smarthome.home.domain.models
+
+data class DeviceType(
+    val id: Int,
+    val name: String,
+)
---
+++ composeApp/src/commonMain/kotlin/id/co/hasilkarya/smarthome/home/domain/models/Room.kt
@@ -0,0 +1,6 @@
+package id.co.hasilkarya.smarthome.home.domain.models
+
+data class Room(
+    val id: Int,
+    val name: String,
+)
